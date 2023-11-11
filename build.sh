726d297509d4b005d2840097cbd92dedcf389acb
rybin.gleb <32393520+RybinGlebRG@users.noreply.github.com>
2023-11-11 17:03:06 +0300

    Added build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "ERROR: Parameter is not set"
+  exit 1
+fi
+
+if [ -z "$JAVA_HOME" ]; then
+  echo "ERROR: JAVA_HOME is not set"
+  exit 1
+fi
+
+chmod u+x ./mvnw && \
+REGISTRY_URL="$1" && \
+APP_VERSION=v$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)   && \
+APP_NAME=$(./mvnw help:evaluate -Dexpression=project.name -q -DforceStdout) && \
+./mvnw clean install -Dmaven.test.skip && \
+docker build --no-cache -t "$APP_NAME:$APP_VERSION" -f ./Dockerfile . && \
+docker image tag "$APP_NAME:$APP_VERSION"  "$REGISTRY_URL/$APP_NAME:$APP_VERSION" && \
+docker image push "$REGISTRY_URL/$APP_NAME:$APP_VERSION"
