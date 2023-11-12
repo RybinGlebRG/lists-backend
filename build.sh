3baf6704c8c9f48cea21999c5f2bc621caf6a8ac
rybin.gleb <32393520+RybinGlebRG@users.noreply.github.com>
2023-11-12 18:20:06 +0300

    Fixing build script

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ chmod u+x ./mvnw && \
 REGISTRY_URL="$1" && \
 APP_VERSION=v$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)   && \
 APP_NAME=$(./mvnw help:evaluate -Dexpression=project.name -q -DforceStdout) && \
+docker manifest inspect "$REGISTRY_URL/$APP_NAME:$APP_VERSION" || \
 ./mvnw clean install -Dmaven.test.skip && \
 docker build --no-cache -t "$APP_NAME:$APP_VERSION" -f ./Dockerfile . && \
 docker image tag "$APP_NAME:$APP_VERSION"  "$REGISTRY_URL/$APP_NAME:$APP_VERSION" && \
