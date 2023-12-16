08d81cfd0ba60c092714cab5b16362daebb0eff8
rybin.gleb <32393520+RybinGlebRG@users.noreply.github.com>
2023-12-16 21:11:21 +0300

    Added book status filters

--- src/main/java/ru/rerumu/lists/model/books/Search.java
+++ src/main/java/ru/rerumu/lists/model/books/Search.java
@@ -3,20 +3,20 @@ package ru.rerumu.lists.model.books;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Search {
-
-    private final List<SortItem> sortItemList;
-    private final Boolean isChainBySeries;
+public record Search(List<SortItem> sortItemList, List<Filter> filters, Boolean isChainBySeries) {
 
     @JsonCreator
     public Search(
             @JsonProperty("sort") List<SortItem> sortItemList,
+            @JsonProperty("filters")  List<Filter> filters,
             @JsonProperty("isChainBySeries") Boolean isChainBySeries
     ){
-        this.sortItemList = sortItemList;
+        this.sortItemList = sortItemList != null ? new ArrayList<>(sortItemList) : null;
         this.isChainBySeries = isChainBySeries != null ? isChainBySeries : false;
+        this.filters = filters != null ? new ArrayList<>(filters) : new ArrayList<>();
     }
 
     public List<SortItem> getSortItemList() {
